c0645a5f7fb4996304b2e41a33272a3d5869c0f4
Valeriy Zamaraiev <v@1kx.capital>
2024-02-23 14:51:45 +0100

    fix Guard deployment

--- pointsNft/ignition/modules/points.ts
+++ pointsNft/ignition/modules/points.ts
@@ -4,7 +4,7 @@ export default buildModule("Points", (m) => {
     const safeSingleton = "0xd9db270c1b5e3bd161e8c8503c55ceabee709552"
     const safeFactory = "0xa6b71e26c5e0845f74c812102ca7114b6a896ab2"
     const pointsSafeModule = m.contract("PointsSafeModule", []);
-    const pointsSafeGuard = m.contract("PointsSafeGuard", []);
+    const pointsSafeGuard = m.contract("PointsSafeGuard", [pointsSafeModule]);
     const pointsNftContract = m.contract("Points", [safeSingleton, safeFactory, pointsSafeModule, pointsSafeGuard]);
     return { pointsNftContract, pointsSafeModule, pointsSafeGuard };
 });
